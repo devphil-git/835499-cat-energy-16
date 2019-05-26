fe736a5e2df601373e7759599a27d5dc6c5f25e8
philipanda <philipanda@mail.ru>
2019-05-26 21:58:30 +0300

    Final1-1

--- gulpfile.js
+++ gulpfile.js
@@ -7,38 +7,6 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-
-gulp.task("css", function () {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
-});
-
-gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
-});
-
-gulp.task("start", gulp.series("css", "server"));
-
-
-/*  ДОПОЛНИТЕЛЬНЫЕ СКРИПТЫ ДЛЯ ЗАПУСКА В BUILD  */
-
 var del = require("del");
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
